bb99ca2146d8e16943355a9384946ac1cecd0c3b
atm0001 <atm0001@example.com>
2015-12-27 05:20:21 +0900

    c

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151226191848) do
+ActiveRecord::Schema.define(version: 20151226194659) do
 
   create_table "messages", force: :cascade do |t|
     t.integer  "to_user_id"
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 20151226191848) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.string   "room_name"
+    t.string   "create_user_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
   create_table "users", force: :cascade do |t|
