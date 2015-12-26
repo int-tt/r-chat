0953079b5a0231c69b3a74bdcbcf04ec95301d0f
atm0001 <atm0001@example.com>
2015-12-27 07:13:07 +0900

    s

--- app/controllers/room_controller.rb
+++ app/controllers/room_controller.rb
@@ -1,2 +1,15 @@
 class RoomController < ApplicationController
+  def create
+    @room = Room.new(message_params)
+
+    respond_to do |format|
+      if @room.save
+        format.html { redirect_to @message, notice: 'Test was successfully created.' }
+        format.json { render :show, status: :created, location: @message }
+      else
+        format.html { render :new }
+        format.json { render json: @message.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end
