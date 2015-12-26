528c044357ac5d3c4246683f1328f43001354e21
k4zzk <kosuke19952000@gmail.com>
2015-12-27 05:12:10 +0900

    users

--- db/migrate/20151226191821_create_users.rb
+++ db/migrate/20151226191821_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string  :name
-      t.string  :email
-      t.string  :password_digest
-      t.boolean :admin
+      t.string  :name,            null: false
+      t.string  :email,           null: false
+      t.string  :password_digest, null: false
+      t.boolean :admin,           null: false, default: false
 
       t.timestamps null: false
     end
--- db/migrate/20151226191831_create_room_members.rb
+++ db/migrate/20151226191831_create_room_members.rb
@@ -3,7 +3,7 @@ class CreateRoomMembers < ActiveRecord::Migration
     create_table :room_members do |t|
       t.integer :room_id
       t.integer :from_user_id
-      t.string :message
+      t.string  :message
 
       t.timestamps null: false
     end
