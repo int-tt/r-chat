fed753cb45955335f632ef33c97397383e7036b3
atm0001 <atm0001@example.com>
2015-12-27 13:10:50 +0900

    s

--- app/controllers/room_controller.rb
+++ app/controllers/room_controller.rb
@@ -4,16 +4,21 @@ class RoomController < ApplicationController
   end
 
   def create
-    @room = Room.new(message_params)
+    userId = 2
+    roomName = params[:roomName]
+    members = params[:members]
 
-    respond_to do |format|
-      if @room.save
-        format.html { redirect_to @message, notice: 'Test was successfully created.' }
-        format.json { render :show, status: :created, location: @message }
-      else
-        format.html { render :new }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
+
+    room = Room.create(:room_name => roomName, :create_user_id => userId)
+
+    unless room.new_record?
+      id = room.id
+      members.each do |member|
+        RoomMember.create(:room_id => id, :user_id => userId)
       end
+    else
+
     end
+  render :text => "aaa"
   end
 end
